0f031af491f2cd481cc9281718acdec0affffa8b
Joe <joe.syx@gmail.com>
2022-03-23 14:51:16 +1100

    Reset the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React, { useState } from 'react'
-import DUMMY_EXPENSES from './data/DummyExpenses';
-import Expenses from './components/Expenses/Expenses';
-import NewExpense from './components/NewExpense/NewExpense';
-
-import './App.css';
-
-const App = () => {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
-
-  const addExpenseHandler = expense => {
-    setExpenses((preExpenses) => {
-      return [expense, ...preExpenses]
-    })
-  }
-
+function App() {
   return (
-    <div className="App">
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses} />
+    <div>
+      <h2>Let's get started!</h2>
     </div>
   );
 }
