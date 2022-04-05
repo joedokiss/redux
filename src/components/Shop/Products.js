4239f33e06ccf8ef106428a2c934c5b14937b963
Joe <joe.syx@gmail.com>
2022-04-05 21:38:20 +1000

    Wrap up the cart feature.

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,40 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 1,
+    title: 'First product',
+    description: 'This is the first product',
+    price: 10.02
+  },
+  {
+    id: 2,
+    title: 'Second product',
+    description: 'This is the second product',
+    price: 5.25
+  },
+  {
+    id: 3,
+    title: 'Third product',
+    description: 'This is the third product',
+    price: 8.99
+  }
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description} />
+        ))}
       </ul>
     </section>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,10 @@ import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./slices/cart-slice";
 import uiSlice from "./slices/ui-slice";
 
-
 const store = configureStore({
   reducer: {
-    uiReducer: uiSlice.reducer,
-    cartReducer: cartSlice.reducer
+    ui: uiSlice.reducer,
+    cart: cartSlice.reducer
   }
 })
 
